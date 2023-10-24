caf1230b7c0b58178d26290e8331463aa37b7a78
Matthew Hess <matthew9510@gmail.com>
2023-10-24 12:20:56 -0700

    BinarySearchTree.ts insert

--- Neetcode/src/Algorithms and Data Structures for Begginers/Trees/Binary Search Tree/BinarySearchTree.ts
+++ Neetcode/src/Algorithms and Data Structures for Begginers/Trees/Binary Search Tree/BinarySearchTree.ts
@@ -16,8 +16,28 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(val: number) {
+        this.root = this.insertNode(this.root, val);
+    }
+
+    insertNode(root: BinarySearchTreeNode | null, val: number) {
+        if (root === null) {
+            return new BinarySearchTreeNode(val);
+        } else if (root.val !== null && val < root.val) {
+            root.left = this.insertNode(root.left, val);
+        } else {
+            root.right = this.insertNode(root.right, val)
+        }
+
+        return root;
+    }
 }
 
 const tree = new BinarySearchTree()
+
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
 
 console.log(tree.root)
