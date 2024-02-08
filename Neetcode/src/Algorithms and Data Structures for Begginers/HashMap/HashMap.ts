653bccba3b8b330010416a02198035698ca1f9f1
Matthew Hess <matthew9510@gmail.com>
2024-02-08 10:28:18 -0800

    updated hash map ds

--- Neetcode/src/Algorithms and Data Structures for Begginers/HashMap/HashMap.ts
+++ Neetcode/src/Algorithms and Data Structures for Begginers/HashMap/HashMap.ts
@@ -1,12 +1,23 @@
-// class HashMap {
-//   state = {};
+class HashMap {
+  private state: { [key: string]: number };
 
-//   constructor() {}
+  constructor() {
+    this.state = {};
+  }
 
-//   function(params:type) {
+  set(key: string, value: number): void {
+    this.state[key] = value;
+  }
 
-//   }
-// }
+  get(key: string): number | undefined {
+    return this.state[key];
+  }
 
-const hash: Object: Number = {};
-hash["a"] = 100;
+  has(key: string): boolean {
+    return this.state.hasOwnProperty(key);
+  }
+}
+
+const hashMap = new HashMap();
+hashMap.set("a", 100);
+console.log(hashMap.get("a")); // Output: 100
